be63ddfdd3af2ce13756a6d81778705f7e6d623e
Hanoz <hanoz.ichhaporia@gmail.com>
2021-06-17 12:49:16 +0530

    day 13 changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
-    var days = 30;
-    var name = "Hanoz";
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
